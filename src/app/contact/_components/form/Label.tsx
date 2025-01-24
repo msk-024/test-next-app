7902bf71d693d750461a64fdbd924537e26b2bbe
m00nsh <mo3ov.877@gmail.com>
2025-01-24 09:09:43 +0900

    new add

---
+++ src/app/contact/_components/form/Label.tsx
@@ -0,0 +1,15 @@
+// "use client";
+import React from "react";
+
+interface LabelProps {
+  text: string;
+  htmlFor: string;
+}
+
+export const Label: React.FC<LabelProps> = (props) => {
+  return (
+    <label htmlFor={props.htmlFor} className="w-[240px]">
+      {props.text}
+    </label>
+  );
+};
